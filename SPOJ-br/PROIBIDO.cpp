5dc3b68b053d36317be1b87223799a5ea423bdc2
jardelbaia <jardelbaiajr@gmail.com>
2020-08-04 17:28:53 -0300

    add SPOJ-BR exs

---
+++ SPOJ-br/PROIBIDO.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main () {
+	int n,x;
+
+	cin >> n;
+	vector<int> k(n);
+	for (long long int a=0; a<n; a++) {
+		cin >> k[a];
+	}
+	sort(k.begin(),k.end());
+	while (cin>>x) {
+		
+		int inicio = 0, fim=n-1;		
+
+		while (inicio<=fim) {
+			int meio = (inicio+fim)/2;
+			if (k[meio] == x) {
+				cout << "sim" << endl;
+				break;
+			}
+			else if (k[meio] >x) fim = meio-1;
+			else inicio = meio+1;
+		}
+		if (inicio>fim) cout << "nao" << endl;
+	}
+	return 0;
+} 
---
+++ SPOJ-br/SALDO.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main () {
+	int n,b=1;
+
+	while(cin>>n, n!=0) {
+		int s=0,x,y,m=0,fend=-1,fbeg=-1,beg=0;
+
+		for (int a=0; a<n; a++) {
+
+			cin >> x >> y;
+		
+			
+			if (x-y+s < 0) {
+				s=0;
+				beg=a+1;
+			} else {
+				s+= x-y;
+			}
+			
+			if (s>m || (s==m && (fend-fbeg)<(a-beg))) {
+				m=s;
+				fend=a;
+				fbeg=beg;
+			}
+		}
+
+		cout << "Teste "<<b++ << endl;
+	
+		if (m>0)
+			cout << fbeg+1<< " "<<fend+1<<endl;
+		else 
+			cout << "nenhum"<<endl;
+		
+		cout << endl;
+	}			
+		
+	return 0;
+} 
